a88e0c783a0f5b538375c2c4bf91a1d94a3ec5bc
John Lam <jflam@microsoft.com>
2022-12-06 23:11:11 -0800

    I have the Jupyter UX I want CTRL+ENTER overloaded to send to ChatGPT

--- src/background/inject.js
+++ src/background/inject.js
@@ -1,22 +1,60 @@
 // This code will be injected by creating an element on the page and 
 // injecting a script element onto the page so that I can access the
-// existing loaded CodeMirror library that is in the page.
+// existing Jupyter page code.
 
 (function() {
+	// Register a custom action that will either execute the 
+	// current cell or pop an alert (simulate send to GPT)
+	var send_to_chatgpt = function () {
+		var cell = Jupyter.notebook.get_selected_cell();
+		if (cell.cell_type == 'code') {
+			const code = cell.get_text();
+			if (code.startsWith('%chat')) {
+				// TODO: actually send code to chat gpt (along with previous cells as context)
+				console.log(`Send to ChatGPT ${code}`);
 
-	// Do all of this only if we detect that this is a Jupyter page
-	var e = document.getElementsByClassName('selected');
-	if (e != null) {
-		for (let i = 0; i < e.length; i++) {
-			// find the descendent element that has classname "CodeMirror"
-			editor = e[i].querySelector(".CodeMirror-code");
-			if (editor != null) {
-				console.log(e[i].className)
-				console.log(editor.className)
-				console.log(editor.innerText)
+				// TODO: receive and parse response into markdown and code
+
+				// TODO: remove test harness
+				const markdown1 = "## This is a markdown cell";
+				const code1 = "print('hello world')";
+				const markdown2 = "## This is another markdown cell";
+
+				// Insert test harness into page
+				Jupyter.notebook.insert_cell_below();
+				Jupyter.notebook.select_next();
+				Jupyter.notebook.cells_to_markdown();
+				Jupyter.notebook.get_selected_cell().set_text(markdown1);
+				Jupyter.notebook.get_selected_cell().render();
+
+				Jupyter.notebook.insert_cell_below();
+				Jupyter.notebook.select_next();
+				Jupyter.notebook.get_selected_cell().set_text(code1);
+
+				Jupyter.notebook.insert_cell_below();
+				Jupyter.notebook.select_next();
+				Jupyter.notebook.cells_to_markdown();
+				Jupyter.notebook.get_selected_cell().set_text(markdown2);
+				Jupyter.notebook.get_selected_cell().render();
+			} else {
+				cell.execute();
 			}
+		} else {
+			console.log("No selected cell");
 		}
-	}
+	};
 
-	alert('inserted self... giggity');
+	// TODO: figure out what real values to use for these
+	var action = {
+		icon: 'fa-comment', // a font-awesome class used on buttons, etc
+		help    : 'Send to ChatGPT',
+		help_index : 'zz',
+		handler : send_to_chatgpt
+	};
+	var prefix = 'auto';
+	var action_name = 'send-to-chatgpt';
+	var full_action_name = Jupyter.actions.register(action, action_name, prefix); // returns 'auto:send-to-chatgpt'
+	Jupyter.notebook.keyboard_manager.edit_shortcuts.add_shortcut('ctrl-enter', full_action_name);
+
+	console.log('Im in yr page, injecting scripts');
 })();
